d20c2bb045a405d86ab2d36b7e754e6047c757be
Ridhima <aroraridhima1001@gmail.com>
2024-09-08 09:37:42 +0530

    Home Page Major Changes

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import Hero from "../../sections/Hero/Hero";
+import OurFunctionality from "../../sections/OurFunctionality/OurFunctionality";
+import PerformanceMetrics from "../../sections/PerformanceMetrics/PerformanceMetrics";
+import './Home.css';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <Hero />
+            <PerformanceMetrics />
+            <OurFunctionality />
+        </div>
+    )
+}
+
+export default Home;
